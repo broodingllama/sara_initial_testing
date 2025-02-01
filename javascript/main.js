365f839ee44dfa4552900d38ca6e45bac956177f
Charles Shaviro <cshaviro@gmail.com>
2025-02-01 15:59:57 -0800

    initial landing page commit

---
+++ javascript/main.js
@@ -0,0 +1,18 @@
+// Example manual click tracking
+function trackClick(linkName) {
+  console.log("User clicked on: " + linkName);
+  // If using Google Analytics, you can also log an event:
+  gtag('event', 'click_link', {
+    'event_category': 'navigation',
+    'event_label': linkName
+  });
+}
+
+// Track CTA (Call To Action) button clicks
+function trackCTA(buttonName) {
+  console.log("CTA button clicked: " + buttonName);
+  gtag('event', 'cta_click', {
+    'event_category': 'button',
+    'event_label': buttonName
+  });
+}
